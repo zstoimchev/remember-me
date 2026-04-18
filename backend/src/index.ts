0bbd515b802967b9494e88b563593c8bd4591cdd
Zhivko Stoimchev <zstoimchev@outlook.com>
2026-04-18 14:40:06 +0200

    init: backend

---
+++ backend/src/index.ts
@@ -0,0 +1,19 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello from TypeScript Express 🚀");
+});
+
+app.get("/health", (req: Request, res: Response) => {
+    res.json({ status: "ok" });
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
